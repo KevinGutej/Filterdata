db409d24bdeacda91598d28f02c4bbc610e268ab
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-05-09 22:39:24 +0200

    Initial Commit

---
+++ FilterData/loginForm.php
@@ -0,0 +1,16 @@
+<html>
+<body> 
+<form action="login.php" method="POST">
+    <div>
+      Username: <input type="text" name="user" required>
+  </div>
+  <div>
+      Password: <input type="password" name="password" required>
+  </div>
+  <div>
+      <input type="submit" value="Login">
+  </div>
+    </form>
+</body>
+
+</html>
