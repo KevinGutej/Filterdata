ae7532789c155046cd5ff8a9a6aac8e5e99932dc
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-05-11 22:35:36 +0200

    Making a filter for sql injection

--- FilterData/login.php
+++ FilterData/login.php
@@ -1,21 +1,23 @@
 <?php
 
-if(!empty($_POST["user"]) && isset($_POST["user"])) {
-    if(!empty($_POST["password"]) && isset($_POST["password"])) {
-      $connection = new mysqli("localhost", "root" , "" , "system");
-    if($connection->errorno == 0) {
-        $userUsername = $_POST["user"];
+
+if (!empty($_POST["user"]) && isset($_POST["user"])) {
+  if (!empty($_POST["password"]) && isset($_POST["password"])) {
+    $connection = new mysqli("localhost", "root", "", "system");
+    if ($connection->errno == 0) {
+      $userUsername = $_POST["user"];
       $userPassword = $_POST["password"];
       $sanitizedUsername = $connection->real_escape_string($userUsername);
       $sanitizedPassword = $connection->real_escape_string($userPassword);
-      $sqlRequest = "SELECT username, password FROM user_accounts WHERE username = '$sanitizedUsername' AND password = '$sanitizedPassword'";";
+      $sqlRequest = "SELECT username, password FROM user_accounts WHERE username = '$sanitizedUsername' AND password = '$sanitizedPassword';";
       $result = $connection->query($sqlRequest);
-      if($result->num_rows == 1) {
-          header('location:success.php');
+      if ($result->num_rows == 1) {
+        header('location:success.php');
         exit();
-            }else{
-          header('location: loginForm.php');
+      } else {
+        header('location: loginForm.php');
       }
     }
   }
 }
+$sqlRequest = "SELECT username, password FROM user_accounts WHERE username = '1' OR 1=1-- AND password = '$sanitizedPassword';";
